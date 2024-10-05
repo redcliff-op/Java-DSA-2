2809a958506f715b324d12a9695995b5351e327b
redcliff-op <devyashjangid@gmail.com>
2024-10-05 23:08:22 +0530

    Arrays: Unsorted 2 Sum
    https://www.geeksforgeeks.org/problems/key-pair5616/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
    
    Given an array arr of positive integers and another number target. Determine whether two elements exist in arr whose sum is exactly target or not. Return a boolean value true if two elements exist in arr else return false.
    
    Examples:
    
    Input: arr[] = [1, 4, 45, 6, 10, 8], target =16
    Output: true
    Explanation: arr[3] + arr[4] = 6 + 10 = 16
    Input: arr[] = [1, 2, 4, 3, 6], target = 11
    Output: false
    Explanation: None of the pair makes a sum of 11

---
+++ TwoSum.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class TwoSum {
+  public boolean twoSum(int[] arr, int target) {
+    HashSet<Integer> seen = new HashSet<>();
+    for (int num : arr) {
+      int complement = target - num;
+      if (seen.contains(complement)) {
+        return true;
+      }
+      seen.add(num);
+    }
+    return false;
+  }
+}
