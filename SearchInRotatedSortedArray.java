5876e3aeaebe60c8ddae0ec083e4a702e73ca44c
redcliff-op <devyashjangid@gmail.com>
2024-10-06 09:32:51 +0530

    Arrays: Search in Rotated Sorted Array
    https://leetcode.com/problems/search-in-rotated-sorted-array/description/
    
    There is an integer array nums sorted in ascending order (with distinct values).
    
    Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
    
    Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
    
    You must write an algorithm with O(log n) runtime complexity.
    
    Example 1:
    
    Input: nums = [4,5,6,7,0,1,2], target = 0
    Output: 4
    Example 2:
    
    Input: nums = [4,5,6,7,0,1,2], target = 3
    Output: -1
    Example 3:
    
    Input: nums = [1], target = 0
    Output: -1

---
+++ SearchInRotatedSortedArray.java
@@ -0,0 +1,25 @@
+public class SearchInRotatedSortedArray {
+  public int search(int[] nums, int target) {
+    int left = 0, right = nums.length - 1, mid;
+    while (left <= right) {
+      mid = (left + right) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      }
+      if (nums[left] <= nums[mid]) {
+        if (nums[left] <= target && nums[mid] >= target) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      } else {
+        if (nums[mid] <= target && nums[right] >= target) {
+          left = mid + 1;
+        }else{
+          right = mid-1;
+        }
+      }
+    }
+    return -1;
+  }
+}
