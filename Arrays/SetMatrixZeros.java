74d659d6ab7cd1e4a43cf94191b6b4729c649ff6
redcliff-op <devyashjangid@gmail.com>
2025-02-23 12:14:03 +0530

    [Arrays] - Set Matrix Zeros
    Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
    
    You must do it in place.
    
    https://leetcode.com/problems/set-matrix-zeroes/description/

---
+++ Arrays/SetMatrixZeros.java
@@ -0,0 +1,56 @@
+public class SetMatrixZeros {
+
+  // Brute Force
+
+  public void setZeroes(int[][] matrix) {
+    int l = matrix.length;
+    int r = matrix[0].length;
+    int[][] temp = new int[l][r];
+    for (int i = 0; i < l; i++) {
+      for (int j = 0; j < r; j++) {
+        temp[i][j] = matrix[i][j]
+      }
+    }
+    for (int i = 0; i < l; i++) {
+      for (int j = 0; j < r; j++) {
+        if (matrix[i][j] == 0) {
+          for (int x = 0; x < r; x++) {
+            temp[i][x] = 0;
+          }
+          for (int x = 0; x < l; x++) {
+            temp[x][j] = 0;
+          }
+        }
+      }
+    }
+    for (int i = 0; i < l; i++) {
+      for (int j = 0; j < r; j++) {
+        matrix[i][j] = temp[i][j];
+      }
+    }
+  }
+
+  // Better
+
+  public void setZeroes2(int[][] matrix) {
+    int l = matrix.length;
+    int r = matrix[0].length;
+    int rowMatrix[] = new int[r];
+    int colMatrix[] = new int[l];
+    for (int i = 0; i < l; i++) {
+      for (int j = 0; j < r; j++) {
+        if (matrix[i][j] == 0) {
+          colMatrix[i] = 1;
+          rowMatrix[j] = 1;
+        }
+      }
+    }
+    for (int i = 0; i < l; i++) {
+      for (int j = 0; j < r; j++) {
+        if (colMatrix[i] == 1 || rowMatrix[j] == 1) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+  }
+}
