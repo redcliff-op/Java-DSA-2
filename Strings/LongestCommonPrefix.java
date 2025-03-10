c7dfa687bcb9d3b6f92ca484636794d4812b94fb
redcliff-op <devyashjangid@gmail.com>
2025-03-10 22:37:49 +0530

    [Strings] - Longest Common Prefix
    Write a function to find the longest common prefix string amongst an array of strings.
    
    If there is no common prefix, return an empty string "".
    
    https://leetcode.com/problems/longest-common-prefix/description/

---
+++ Strings/LongestCommonPrefix.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class LongestCommonPrefix {
+  public String longestCommonPrefix(String[] strs) {
+    String res = strs[0];
+    int len = strs.length;
+    for (int i = 1; i < len; i++) {
+      for (int j = 0; j < res.length(); j++) {
+        if (j >= strs[i].length() || strs[i].charAt(j) != res.charAt(j)) {
+          res = res.substring(0, j);
+        }
+      }
+    }
+    return res;
+  }
+}
